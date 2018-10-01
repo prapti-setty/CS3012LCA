091fe79f7e531f7a55dc82e5de29e4ce5122eab1
Prapti Setty <settyp@tcd.ie>
2018-10-01 16:07:03 +0100

    update tests to include creation of tree in test

--- LCAProblemTest.java
+++ LCAProblemTest.java
@@ -3,23 +3,32 @@ import org.junit.Test;
 
 public class LCAProblemTest 
 {
-
+	public static LCAProblem tree;
+    public static void treeOne()
+    {
+    	tree = new LCAProblem();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+    }
+ 
 	@Test
-	public void testOne() 
+	public void test() 
 	{
-		assertEquals("testing LCA of 4,5", LCAProblem.computeLCA(4,5), 2);
+		treeOne();
+		assertEquals("testing LCA of 4,5", LCAProblem.computeLCA(4,5,tree), 2);
+		assertEquals("testing LCA of 4,6", LCAProblem.computeLCA(4,6,tree), 1);
+		assertEquals("testing LCA of 3,4", LCAProblem.computeLCA(3,4,tree), 1);
 	}
 	
 	@Test
-	public void testTwo() 
+	public void testFalse() 
 	{
-		assertEquals("testing LCA of 4,6", LCAProblem.computeLCA(4,6), 1);
-	}
-	
-	@Test
-	public void testThree() 
-	{
-		assertEquals("testing LCA of 3,4", LCAProblem.computeLCA(3,4), 1);
+		
 	}
 
 }
